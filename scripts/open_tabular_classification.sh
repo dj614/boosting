900a8bd6b00fc1d20feb2ab85c02aafea9a1358d
dj614 <daisyjia106117@gmail.com>
2026-03-13 02:18:53 +0800

    update scripts to rename outdir

--- scripts/open_tabular_classification.sh
+++ scripts/open_tabular_classification.sh
@@ -1,3 +1,3 @@
-python scripts/run_open_tabular_benchmark.py   --classification-datasets blood climate credit diabetes german_numer qsar raisin titanic   --regression-datasets   --n-jobs 12
+python scripts/run_open_tabular_benchmark.py   --classification-datasets blood climate credit diabetes german_numer qsar raisin titanic   --regression-datasets   --n-jobs 12 --outdir outputs/open_tabular_benchmark_v1_classification
 
-python scripts/analyze_open_tabular_benchmark.py   --input-dir outputs/open_tabular_benchmark_v1   --task-types classification
+python scripts/analyze_open_tabular_benchmark.py   --input-dir outputs/open_tabular_benchmark_v1_classification   --task-types classification
--- scripts/open_tabular_regression.sh
+++ scripts/open_tabular_regression.sh
@@ -1,3 +1,3 @@
-python scripts/run_open_tabular_benchmark.py   --classification-datasets   --n-jobs 12
+python scripts/run_open_tabular_benchmark.py   --classification-datasets   --n-jobs 12 --outdir outputs/open_tabular_benchmark_v1_regression
 
-python scripts/analyze_open_tabular_benchmark.py   --input-dir outputs/open_tabular_benchmark_v1   --task-types regression
+python scripts/analyze_open_tabular_benchmark.py   --input-dir outputs/open_tabular_benchmark_v1_regression   --task-types regression

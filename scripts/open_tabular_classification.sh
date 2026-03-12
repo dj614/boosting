b1e9941c3fbf8db2146a6583284ecb0b2cd724a3
dj614 <daisyjia106117@gmail.com>
2026-03-13 02:01:59 +0800

    add overall script for classification & regression

---
+++ scripts/open_tabular_classification.sh
@@ -0,0 +1,3 @@
+python scripts/run_open_tabular_benchmark.py   --classification-datasets blood climate credit diabetes german_numer qsar raisin titanic   --regression-datasets   --n-jobs 12
+
+python scripts/analyze_open_tabular_benchmark.py   --input-dir outputs/open_tabular_benchmark_v1   --task-types classification
---
+++ scripts/open_tabular_regression.sh
@@ -0,0 +1,3 @@
+python scripts/run_open_tabular_benchmark.py   --classification-datasets   --n-jobs 12
+
+python scripts/analyze_open_tabular_benchmark.py   --input-dir outputs/open_tabular_benchmark_v1   --task-types regression

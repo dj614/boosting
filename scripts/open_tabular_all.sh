6e29ba698031d324fbc644f87d3e442589976d88
dj614 <daisyjia106117@gmail.com>
2026-03-14 09:45:04 +0800

    update .sh

--- scripts/open_tabular_all.sh
+++ scripts/open_tabular_all.sh
@@ -1,15 +1,35 @@
-python scripts/run_open_tabular_benchmark.py --classification-datasets blood climate credit diabetes german_numer qsar raisin titanic --regression-datasets california_housing concrete_compressive_strength superconductivity diamonds --n-jobs 4 --outdir outputs/open_tabular_benchmark_loss_report --use-report-metric-for-selection --ctb-target-modes loss_aware
+# # classification
 
-python scripts/analyze_open_tabular_benchmark.py --input-dir outputs/open_tabular_benchmark_loss_report
+# python scripts/run_open_tabular_benchmark.py --classification-datasets blood climate credit diabetes german_numer qsar raisin titanic --regression-datasets   --n-jobs 12 --outdir outputs/open_tabular_benchmark_classification_loss_report --use-report-metric-for-selection --ctb-target-modes loss_aware
 
-python scripts/run_open_tabular_benchmark.py --classification-datasets blood climate credit diabetes german_numer qsar raisin titanic --regression-datasets california_housing concrete_compressive_strength superconductivity diamonds --n-jobs 4 --outdir outputs/open_tabular_benchmark_loss_logloss --ctb-target-modes loss_aware
+# python scripts/analyze_open_tabular_benchmark.py --input-dir outputs/open_tabular_benchmark_classification_loss_report --task-types classification
 
-python scripts/analyze_open_tabular_benchmark.py --input-dir outputs/open_tabular_benchmark_loss_logloss
+# python scripts/run_open_tabular_benchmark.py --classification-datasets blood climate credit diabetes german_numer qsar raisin titanic --regression-datasets   --n-jobs 12 --outdir outputs/open_tabular_benchmark_classification_loss_logloss --ctb-target-modes loss_aware
 
-python scripts/run_open_tabular_benchmark.py --classification-datasets blood climate credit diabetes german_numer qsar raisin titanic --regression-datasets california_housing concrete_compressive_strength superconductivity diamonds --n-jobs 4 --outdir outputs/open_tabular_benchmark_legacy_report --use-report-metric-for-selection
+# python scripts/analyze_open_tabular_benchmark.py --input-dir outputs/open_tabular_benchmark_classification_loss_logloss --task-types classification
 
-python scripts/analyze_open_tabular_benchmark.py --input-dir outputs/open_tabular_benchmark_legacy_report
+# python scripts/run_open_tabular_benchmark.py --classification-datasets blood climate credit diabetes german_numer qsar raisin titanic --regression-datasets   --n-jobs 12 --outdir outputs/open_tabular_benchmark_classification_legacy_report --use-report-metric-for-selection
 
-python scripts/run_open_tabular_benchmark.py --classification-datasets blood climate credit diabetes german_numer qsar raisin titanic --regression-datasets california_housing concrete_compressive_strength superconductivity diamonds --n-jobs 4 --outdir outputs/open_tabular_benchmark_legacy_logloss
+# python scripts/analyze_open_tabular_benchmark.py --input-dir outputs/open_tabular_benchmark_classification_legacy_report --task-types classification
 
-python scripts/analyze_open_tabular_benchmark.py --input-dir outputs/open_tabular_benchmark_legacy_logloss
+# python scripts/run_open_tabular_benchmark.py --classification-datasets blood climate credit diabetes german_numer qsar raisin titanic --regression-datasets   --n-jobs 12 --outdir outputs/open_tabular_benchmark_classification_legacy_logloss
+
+# python scripts/analyze_open_tabular_benchmark.py --input-dir outputs/open_tabular_benchmark_classification_legacy_logloss --task-types classification
+
+# regression
+
+python scripts/run_open_tabular_benchmark.py --classification-datasets   --regression-datasets california_housing concrete_compressive_strength superconductivity diamonds --n-jobs 12 --outdir outputs/open_tabular_benchmark_regression_loss_report --use-report-metric-for-selection --ctb-target-modes loss_aware
+
+python scripts/analyze_open_tabular_benchmark.py --input-dir outputs/open_tabular_benchmark_regression_loss_report  --task-types regression
+
+python scripts/run_open_tabular_benchmark.py --classification-datasets   --regression-datasets california_housing concrete_compressive_strength superconductivity diamonds --n-jobs 12 --outdir outputs/open_tabular_benchmark_regression_loss_logloss --ctb-target-modes loss_aware
+
+python scripts/analyze_open_tabular_benchmark.py --input-dir outputs/open_tabular_benchmark_regression_loss_logloss  --task-types regression
+
+python scripts/run_open_tabular_benchmark.py --classification-datasets   --regression-datasets california_housing concrete_compressive_strength superconductivity diamonds --n-jobs 12 --outdir outputs/open_tabular_benchmark_regression_legacy_report --use-report-metric-for-selection
+
+python scripts/analyze_open_tabular_benchmark.py --input-dir outputs/open_tabular_benchmark_regression_legacy_report  --task-types regression
+
+python scripts/run_open_tabular_benchmark.py --classification-datasets   --regression-datasets california_housing concrete_compressive_strength superconductivity diamonds --n-jobs 12 --outdir outputs/open_tabular_benchmark_regression_legacy_logloss
+
+python scripts/analyze_open_tabular_benchmark.py --input-dir outputs/open_tabular_benchmark_regression_legacy_logloss  --task-types regression
